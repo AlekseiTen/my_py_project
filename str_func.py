b2b03dbe3d18bbb54892a87986ed975ada7262ad
aleksei <amailcn@mail.ru>
2024-05-19 23:07:50 +0300

    ф-ция выводит первую букву загл. и добавил докстринг

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def get_upper_word(word:str):
+    """
+
+    :param word:
+    :return: возв все букв заг.
+    """
     return word.upper()
+
+
+def get_title(word: str):
+    """
+
+    :param word:
+    :return: возвращ. закглавную букву
+    """
+    return word.title()
